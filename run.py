c73db2f740e2251c0ff7b3b52d3f7c63a3a9aea7
Ibtisam90 <ibtisam9220@gmail.com>
2025-10-27 14:47:52 +0500

    Remove dev mode

--- run.py
+++ run.py
@@ -1,20 +1,20 @@
 import os
 
-if os.getenv("DEV", False):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "site-to-agent-69c06280f316.json"
+# if os.getenv("DEV", False):
+#     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "site-to-agent-69c06280f316.json"
+# else:
+json_content = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+json_path = "/tmp/google-credentials.json"
+
+if not json_content:
+    raise RuntimeError("Google credentials not found. Please set GOOGLE_APPLICATION_CREDENTIALS_JSON.")
+
+if not os.path.exists(json_path):
+    with open(json_path, "w") as f:
+        f.write(json_content)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
 else:
-    json_content = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
-    json_path = "/tmp/google-credentials.json"
-
-    if not json_content:
-        raise RuntimeError("Google credentials not found. Please set GOOGLE_APPLICATION_CREDENTIALS_JSON.")
-
-    if not os.path.exists(json_path):
-        with open(json_path, "w") as f:
-            f.write(json_content)
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
-    else:
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = json_path
 
 # Now import the rest (after env is set)
 from flask import Flask
